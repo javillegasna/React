686c54430a2ac9da0a07f07a6a018a2b51bc3635
javillegasna <javillegasna@gmail.com>
2022-01-13 13:39:17 -0500

    [TAREA]: Ponerlo Todo Junto :(std : Compela)

--- src/components/PersonCard.js
+++ src/components/PersonCard.js
@@ -1,14 +1,24 @@
 import React, {Component} from "react";
 class PersonCard extends Component{
+    state;
+    constructor(props) {
+        super(props);
+        this.state = {
+            age: props.age
+        };
+    };
+    addAgeByOne=()=>{this.setState({age : this.state.age+1})};
     render(){
-        const {name, lastName, age, hairColor}=this.props
+        const {name, lastName, hairColor}=this.props
         return(
             <section className="PersonCard">
                 <h1 className="PersonCard__Title"> {name}, {lastName} </h1>
-                <p><span>Age:</span>{age}</p>
+                <p><span>Age:</span>{ this.state.age }</p>
                 <p><span>Hair Color:</span>{hairColor}</p>
+                <button className="PersonCard__Button" onClick={this.addAgeByOne} > Birthday Button for {name} {lastName}</button>
             </section>
         )
     };
+    
 }
 export default PersonCard;
