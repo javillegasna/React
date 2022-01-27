1d8569fe616dc1c32ea1c0120048750d0bd86733
javillegasna <javillegasna@gmail.com>
2022-01-27 16:40:13 -0500

    [TAREA] Api Walker (std:incompleta)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import { useState } from "react";
+import Card from "./components/Card";
 import FrmApiWalker from "./components/FrmApiWalker";
 
 function App() {
+  const [cardData, setCardData] = useState({});
   return (
-    <main className="App">
-      <FrmApiWalker/>
+   <main className="App">
+      <FrmApiWalker setCardData={setCardData}/>
+      <Card cardData={cardData}/>
     </main>
   );
 }
--- src/components/FrmApiWalker.js
+++ src/components/FrmApiWalker.js
@@ -1,26 +1,49 @@
-import axios from "axios"
+import axios from "axios";
 import { useEffect, useState } from "react";
-const FrmApiWalker = () => {
-    const [categories, setCategories] = useState([]);
-    useEffect(() => {
-        const catagoriesList = getData();
-        setCategories(Object.keys(catagoriesList))
-    }, []);
-    const getData= async({id=""})=>{
-        const API_URL="https://swapi.dev/api/";
-        const list = await axios.get(API_URL+id)
-        .then(response=>response.data)
-        .catch(error=>error);
-        console.log(list);
-        return list
-    }
-    return (
-        <form>
-            <label htmlFor="categories"> Search for: </label>
-            <select name="categories" id="categories" onChange={()=>getData()}>
-                {categories.map(category=><option value={category}> {category} </option>)}
-            </select>
-        </form>
-     );
-}
+
+const FrmApiWalker = ({setCardData}) => {
+  //Operative const
+  const API_URL = "https://swapi.dev/api/";
+  //States of component
+  const [categories, setCategories] = useState([]);
+  const [currentCategory, setCurrentCategory] = useState("people");
+  const [id, setId] = useState("1");
+
+  //Utilities
+  const getData = async (query = "") =>
+    axios
+      .get(API_URL + query)
+      .then((response) => response.data)
+      .catch((error) => console.error(error));
+
+  //Handlers
+  const handlerId = (event)=>{
+    const{value}=event.target;
+    setId(value)
+  }
+  const handlerSearch=(query)=>(event)=>{
+    getData(query).then(obj=>setCardData(obj));
+  }
+  const handlerCurrentCategory = (event)=>setCurrentCategory(event.target.value)
+
+ //Effects
+  useEffect(() => {
+    getData().then((categories) => setCategories(Object.keys(categories)));
+  }, []);
+  return (
+    <form>
+      <label htmlFor="categories"> Search for: </label>
+      <select name="categories" id="categories" onChange={handlerCurrentCategory}>
+        {categories.map((category, indx) => (
+          <option key={`category${indx}`} value={category}>
+            {category}
+          </option>
+        ))}
+      </select>
+      <label htmlFor="inputId">id: </label>
+      <input type="number" name="inputId" id="inputId" placeholder="1" value={id} onChange={handlerId} />
+      <input type="button" value="Search" onClick={handlerSearch(`${currentCategory}/${id}`)} />
+    </form>
+  );
+};
 export default FrmApiWalker;
